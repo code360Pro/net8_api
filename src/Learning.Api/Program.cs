293acd2483e637f9731483f9794173fef1b528b2
rashid mp <rashid.parambil@outlook.com>
2025-02-16 11:03:34 +0000

    Updated serilog

--- src/Learning.Api/Program.cs
+++ src/Learning.Api/Program.cs
@@ -24,47 +24,28 @@ Log.Information("Serilog has started.....!");
 // Add services to the container.
 
 builder.Services.AddControllers();
-Log.Information("Serilog AddControllers completed.....!");
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-Log.Information("Serilog AddEndpointsApiExplorer completed.....!");
 
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddSwaggerGen();
-    Log.Information("Serilog AddSwaggerGen completed.....!");
 }
 builder.Services.AddHealthChecks(); // Add health checks
 
 
 var app = builder.Build();
-Log.Information("Serilog Build completed.....!");
-
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    Log.Information("Serilog IsDevelopment completed.....!");
 }
-
 app.UseHttpsRedirection();
-
-Log.Information("Serilog UseHttpsRedirection completed.....!");
-
-
 app.UseAuthorization();
-
-Log.Information("Serilog UseAuthorization completed.....!");
-
 app.MapControllers();
-Log.Information("Serilog MapControllers completed.....!");
-
 app.MapHealthChecks("/health");
 
 app.Run();
-Log.Information("Serilog Run completed.....!");
